12e00be5ed35500e1dfb126849256c16ee0c8aec
Pizza-Dox <diamond.nigel75@Gmail.com>
2013-12-21 18:04:23 +0200

    Build 11
    Minor bug fixes and testing some new coding, however errors are still
    chucked everywhere and the script still breaks... :/

--- ACM_Installer_v1.0.sh
+++ ACM_Installer_v1.0.sh
@@ -31,13 +31,11 @@
 # * Build 10:                                                                #
 # - Rebuilt code layout                                                      #
 # - Fixed many stupid bugs                                                   #
+# * Build 11:                                                                #
+# - Bug Fixes                                                                #
+# - Testing new features                                                     #
 ##############################################################################
-sleep 1;
-clear
-echo " ";
-title=ACM Installer V1 Alpha B10
-while true; do
-$title
+echo "ACM Installer V1 Alpha B11";
 echo " ";
 echo "Options:"
 echo " ";
@@ -47,14 +45,14 @@ echo "[R]eboot ACM";
 echo "[E]xit ACM";
 echo " "
 echo -n "What would you like to do? "
-read Install_Options
-echo " "
-case "$Install_Options" in
+while true; do
+	read i
+	case "$i" in
 	i|I)
+	clear
 	echo "Installing ACM...";
 cat >> /mnt/sdcard/acm_temp.sh <<EOF
 #!/system/bin/sh
-while true; do
 echo "Adreno Catalyst Manager Version 1.0";
 echo "by Pizza_Dox";
 sleep 0.5;
@@ -67,35 +65,45 @@ echo "[Q]ptimize GPU Settings for Quality";
 echo "[E]xit";
 echo "[R]eboot";
 echo -n "What would you like to do?";
-read Control_Options
-case "$Control_Options" in
+while true; do
+	read i
+	case "$i" in
 	c|C)
+	clear
 	echo "GPU Clock speed available:";
 	echo "[1] 200 MHz";
 	echo "[2] 320 MHz";
 	echo "[3] 389 MHz";
 	echo "[4] 450 MHz";
-	read gpuclkinput
-	sleep 1;
-	case "$gpuclkinput" in
+	while true; do
+	read i
+	case "$i" in
 	echo -n "Choose clock speeds:";
 	1|1)
+	clear
 	echo "GPU Value injected!";
 	echo "200000000" > /sys/class/kgsl/kgsl/kgsl-3d0/max_gpuclk
 	2|2)
+	clear
 	echo "GPU Value injected!";
 	echo "320000000" > /sys/class/kgsl/kgsl/kgsl-3d0/max_gpuclk
 	3|3)
+	clear
 	echo "GPU Value injected!";
 	echo "389000000" > /sys/class/kgsl/kgsl/kgsl-3d0/max_gpuclk
 	4|4)
+	clear
 	echo "GPU Value injected!";
 	echo "450000000" > /sys/class/kgsl/kgsl/kgsl-3d0/max_gpuclk
+	esac
+	done
 	d|D)
+	clear
 	echo "vRam cache dropped!";
 	sync;
 	echo "3" > /proc/sys/vm/drop_caches;
 	p|P)
+	clear
 	echo "GPU Settings set for Performance!";
 	setprop hw3d.force 1
 	setprop debug.egl.hw 1
@@ -112,12 +120,15 @@ case "$Control_Options" in
 	echo "GPU Settings set for Quality!";
 	setprop persist.sys.use_dithering 1
 	r|R)
+	clear
 	reboot
 	;;
 	e|E)
+	clear
 	echo "Buh Bye!";
 	exit 0
 	*)
+	clear
     echo " "
     echo "Dude! U Drunk?"
     sleep 2
@@ -137,18 +148,21 @@ EOF
 	echo "Installation complete, a reboot is suggested!";
 	;;
 	u|U)
+	clear
 	echo "?Un-installing ACM...";
 	sync; busybox mount -o remount,rw /system
 	rm /system/xbin/acm 2>/dev/null;
 	echo "Un-install complete, a reboot is suggested!";
 	;;
 	e|E)
+	clear
 	echo "Buh Bye!";
 	exit 0
-	*)
 	r|R)
+	clear
 	reboot
 	;;
+	*)
     echo " "
     echo "Dude! U Drunk?"
     sleep 2
